2e5c7063bb507b7533950af26e73737fc0c19553
Aaron Kirkman <kirkman.aaron.w+github@gmail.com>
2019-12-01 13:30:29 -0600

    add xscreensaver lock command

--- config.h
+++ config.h
@@ -65,7 +65,9 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *pastecmd[] = { "xdotool-xclip-paste", NULL};
+static const char *lockcmd[] = { "xscreensaver-command", "-lock", NULL};
 static const char *termcmd[]  = { "st", NULL };
+
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -74,6 +76,7 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_k,      movestack,      {.i = -1 } },
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ WINKEY,                       XK_l,      spawn,          {.v = lockcmd } },
 	{ WINKEY,                       XK_v,      spawn,          {.v = pastecmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
