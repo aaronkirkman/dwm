f8d7e9a118196c833a58d64b6e3e954e0a754693
Aaron Kirkman <kirkman.aaron.w+github@gmail.com>
2019-12-02 07:59:33 -0600

    remove rule for Gimp

--- config.h
+++ config.h
@@ -32,7 +32,6 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Chromium",  NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
